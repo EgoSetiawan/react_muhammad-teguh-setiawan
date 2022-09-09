45924cbd3f46c97be6bce01161223e7f6f543a26
Muhammad Teguh <muhammadteguh1126@gmail.com>
2022-09-09 21:15:39 +0700

    Task/Javascript: membetulkan code style

--- 08_Javascript_Refreshment/praktikum/1.js
+++ 08_Javascript_Refreshment/praktikum/1.js
@@ -1,34 +1,33 @@
-let a = 5
-let b = 'kampus merdeka'
-const NAMA = 'budi'
-let TerdafTar = true
-let LengKap = [a,b,NAMA,TerdafTar]
-let TakeArray = LengKap[2]
+let numberA = 5;
+let kampusMerdeka = "kampus merdeka";
+const NAMA = "budi";
+let TerdafTar = true;
+let LengKap = [a, b, NAMA, TerdafTar];
+let TakeArray = LengKap[2];
 
-function perkenalan(){
-    let asal = 'Indonesia'
-    return console.log(
-        'perkenalkan nama saya' +
-        NAMA +
-        'nomor urut' + 
-        a + 
-        'sekarang sedang mengikuti' +
-        b +
-        'berasal dari' +
-        asal
-    )
+function perkenalan() {
+  let asal = "Indonesia";
+  return console.log(
+    "perkenalkan nama saya" +
+      NAMA +
+      "nomor urut" +
+      a +
+      "sekarang sedang mengikuti" +
+      b +
+      "berasal dari" +
+      asal
+  );
 }
 
 if (TerdafTar === true) {
-    console.log(NAMA + 'terdaftar sebagai kegiatan kampus merdeka')
+  console.log(NAMA + "terdaftar sebagai kegiatan kampus merdeka");
 }
 
-a = b
+a = b;
 // nama = b;
 
-
 //console.log("asal diakses = " + asal);
-console.log(`Mengambil Index Array Ke-2 ${TakeArray}`)
-console.log(`a adalah = ${a}`)
-console.log(`b adalah = ${b}`)
-perkenalan()
+console.log(`Mengambil Index Array Ke-2 ${TakeArray}`);
+console.log(`a adalah = ${numberA}`);
+console.log(`b adalah = ${kampusMerdeka}`);
+perkenalan();
--- 08_Javascript_Refreshment/praktikum/5.js
+++ 08_Javascript_Refreshment/praktikum/5.js
@@ -1,12 +1,12 @@
-const Val = [1,2,3,4,5,6]
+const Val = [1, 2, 3, 4, 5, 6];
 // With ES6
-const NewVal = Val.map((newVal) => newVal * 2)
-console.log(NewVal)
+const NewVal = Val.map((newVal) => newVal * 2);
+console.log(NewVal);
 // Manual With Lopp
-let vaLoop = [] 
-let value = 1
-for(let i = 0; i < val.length; i++){
-    value = Val[i] * 2
-    vaLoop.push(value)
+let vaLoop = [];
+let value = 1;
+for (let i = 0; i < Val.length; i++) {
+  value = Val[i] * 2;
+  vaLoop.push(value);
 }
-console.log(vaLoop)
+console.log(vaLoop);
