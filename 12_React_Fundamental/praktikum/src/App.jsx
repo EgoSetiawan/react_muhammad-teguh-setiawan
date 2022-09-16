3ee8e1cee539b2f5ec4a8672aadfb514e374207a
Muhammad Teguh <muhammadteguh1126@gmail.com>
2022-09-16 23:10:14 +0700

    Task/React_Fundamental: add praktikum

---
+++ 12_React_Fundamental/praktikum/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./component/Navbar/Navbar.jsx";
+import Todo from "./pages/Todo.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
