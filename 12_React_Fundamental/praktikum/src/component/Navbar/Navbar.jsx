2e2ca38f8af8f4628aa1c1e9e5d2d9a34e4d2e7a
Muhammad Teguh <muhammadteguh1126@gmail.com>
2022-09-16 22:01:17 +0700

    Task/React_Fundamental: add praktikum

---
+++ 12_React_Fundamental/praktikum/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="nav">
+        <h1 className="navTitle"> Todo App </h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
