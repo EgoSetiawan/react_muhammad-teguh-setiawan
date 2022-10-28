71847b0f04c7c286a3ae0f742084f7e866fdd2fa
Muhammad Teguh <muhammadteguh1126@gmail.com>
2022-10-28 22:55:19 +0700

    Task/Subcrisption_GraphQL: add file praktikum

---
+++ 24_Subcrisption_GraphQL/praktikum/src/config/typeDef.js
@@ -0,0 +1,77 @@
+import { gql } from "@apollo/client";
+
+export const getPassengerLists = gql`
+  query getAll {
+    users {
+      id
+      jenisKelamin
+      nama
+      umur
+    }
+  }
+`;
+
+export const subPassengerLists = gql`
+  subscription getPassangerSubsctription {
+    users {
+      id
+      nama
+      umur
+      jenisKelamin
+    }
+  }
+`;
+
+export const getById = gql`
+  query getId($id: Int) {
+    users(where: { id: { _eq: $id } }) {
+      id
+      jenisKelamin
+      nama
+      umur
+    }
+  }
+`;
+
+export const deletePassenger = gql`
+  mutation DeletePass($id: Int!) {
+    delete_users_by_pk(id: $id) {
+      id
+      nama
+      umur
+    }
+  }
+`;
+
+export const addPassenger = gql`
+  mutation addPass($objects: [users_insert_input!]!) {
+    insert_users(objects: $objects) {
+      returning {
+        id
+        jenisKelamin
+        nama
+        umur
+      }
+    }
+  }
+`;
+export const updatePassenger = gql`
+  mutation MutationUpdate(
+    $id: Int
+    $nama: String
+    $umur: Int
+    $jenisKelamin: String
+  ) {
+    update_users(
+      where: { id: { _eq: $id } }
+      _set: { nama: $nama, umur: $umur, jenisKelamin: $jenisKelamin }
+    ) {
+      returning {
+        id
+        nama
+        umur
+        jenisKelamin
+      }
+    }
+  }
+`;
