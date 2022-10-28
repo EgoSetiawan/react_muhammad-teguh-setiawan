ca85a6faa7f9b78876fd8c57bf1072d7c9ce85c2
Muhammad Teguh <muhammadteguh1126@gmail.com>
2022-10-28 22:51:52 +0700

    Task/Subscriptions_GraphQL: add file praktikum

---
+++ 24_Subcrisption_GraphQL/praktikum/src/apollo-client/apollo-client.js
@@ -0,0 +1,43 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import CONST from "../utils/CONST";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: CONST.BASE_URL,
+  headers: {
+    "x-hasura-admin-secret": CONST.ADMIN_SECRET,
+  },
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: CONST.BASE_URL_WS,
+    connectionParams: {
+      headers: {
+        "x-hasura-admin-secret": CONST.ADMIN_SECRET,
+      },
+    },
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
---
+++ 24_Subcrisption_GraphQL/praktikum/src/utils/CONST.js
@@ -0,0 +1,7 @@
+const CONST = {
+  BASE_URL: process.env.REACT_APP_BASE_URL_GRAPHQL,
+  BASE_URL_WS: process.env.REACT_APP_BASE_URL_WS,
+  ADMIN_SECRET: process.env.REACT_APP_ADMIN_SECRET,
+};
+
+export default CONST;
